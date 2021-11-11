deea1356e470aa5b0d5c1d74dd2e19654ea50126
JCPosso <juancamiloposso01@gmail.com>
2021-11-11 02:12:22 -0500

    heroku https

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
-window.$dir="http://socialneighborhood.herokuapp.com/"
+window.$dir="https://socialneighborhood.herokuapp.com/"
 //window.$dir="http://localhost:8080/"
 
 ReactDOM.render(
