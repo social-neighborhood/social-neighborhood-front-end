48a6ef97631009eab548e842f01ee50fbbfd2dec
JCPosso <juancamiloposso01@gmail.com>
2021-11-11 02:02:18 -0500

    to herku

--- src/index.js
+++ src/index.js
@@ -2,8 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
-//window.$dir="http://socialneighborhood.herokuapp.com/"
-window.$dir="http://localhost:8080/"
+window.$dir="http://socialneighborhood.herokuapp.com/"
+//window.$dir="http://localhost:8080/"
 
 ReactDOM.render(
   <App />,
