4f6ea19d2f454840165c742c97da429723bf0e4a
JCPosso <juancamiloposso01@gmail.com>
2021-12-04 11:18:25 -0500

    solucion bug notificaciones

--- src/Components/Rightbar/index.js
+++ src/Components/Rightbar/index.js
@@ -14,23 +14,25 @@ import { indigo } from '@mui/material/colors';
 import './rightbar.css';
 const Rightbar = () => {
     const [rtData, setRTData] =  useState([])
-    const [show, setShow] =  useState(false)
-    const [count, setCount] =  useState(0)
+    const [show, setShow] =  useState(true)
+    const [count, setCount] =  useState(1)
 
     async function  loadDataRT(){
         const suscriber = await db.collection('Post').orderBy("fechaPublicacion", "desc").onSnapshot(querySnapshot =>{
             const posts = []
-            let count =0;
-            for (let i=0;i<rtData.length;i++){
-                if(rtData[i].rol =='Administrador') count++;
-            }
-            setCount(count);
+            let cuenta =0;
             querySnapshot.forEach(documentSnapshot => {
                 posts.push({
                     ...documentSnapshot.data(),
                     key: documentSnapshot.id
                 })
             });
+            posts?.forEach((post)=>{
+                if(post.rol =='Administrador'){
+                cuenta++
+                }
+            })
+            setCount(cuenta)
             setRTData(posts);
         })
         return () => suscriber()
@@ -40,7 +42,7 @@ const Rightbar = () => {
         loadDataRT()
     },[])
     const showPost =()=>{
-        setShow(!show)
+        setCount(count);
     }
     return (
         <div className="drawerRight">
@@ -53,19 +55,23 @@ const Rightbar = () => {
                     alignItems="center"
                     justifyContent="flex-end"
                 ><IconButton style={{backgroundColor: indigo[500], borderRadius: 50 ,width:'60px'}}
-                        onClick={showPost}
-                        >
-                        <Badge color="secondary" badgeContent={count} max={10}>
+                        
+                        >   
+                        { rtData?
+                         <Badge color="secondary" badgeContent={count} max={10}>
                         <MailIcon sx={{ fontSize: 40 }}/>
                         </Badge>
+                        :<div></div>}
+                        
                     </IconButton>
                 </Grid>
                 {show?
                  rtData?.map(function (post) {
                     console.log(post)
-                    if(post.rol =='Administrador')
+                    if(post.rol =='Administrador'){
                     return(
                         <Post key={post.id} data={post} />)
+                    }
                 }):<div></div>
                 }
                
