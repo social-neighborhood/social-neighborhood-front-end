968cfd830af28ae37dc8ed6a0d3a755cdf51244c
JCPosso <juancamiloposso01@gmail.com>
2021-12-09 01:09:47 -0500

    change dir

--- src/index.js
+++ src/index.js
@@ -2,8 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
-//window.$dir="https://socialneighborhood.herokuapp.com/"
-window.$dir="http://20.110.173.228:3000/"
+window.$dir="https://socialneighborhood.herokuapp.com/"
+//window.$dir="http://20.110.173.228:3000/"
 //window.$dir="http://localhost:8080/"
 
 ReactDOM.render(
